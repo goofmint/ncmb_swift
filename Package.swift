fad7b66cb54b51b6167e3f18a3898c6f3ad215b7
Atsushi <atsushi@moongift.jp>
2019-04-11 09:22:17 +0900

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+  name: "NCMB",
+  products: [
+    .library(name: "NCMB", targets: ["NCMB"]),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .target(name: "NCMB", dependencies: []),
+    .testTarget(name: "NCMBTests", dependencies: []),
+  ]
+)
